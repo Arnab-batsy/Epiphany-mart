c80f09606ad902f608bfd80f599eba0ab6d6d6c7
Arnab Kumar Das <78913766+Arnab-batsy@users.noreply.github.com>
2024-01-03 18:42:19 +0530

    Add the Navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import logo from '../Assets/logo.png';
 import cart_icon from '../Assets/cart_icon.png';
+import brand from '../Assets/brand.png'
 
 function Navbar() {
+    const [menu, setMenu]= useState("shop");
   return (
     <div className="navbar">
     <div className='nav-logo'>
-        <img src={logo} alt='logo'/>
-        <p>Epiphany Mart</p>
+        <img id="bag" src={logo} alt='logo'/>
+        <p><img src={brand} width="130px" alt='logo'/></p>
     </div>
     <ul className='nav-menu'>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li onClick={()=>{setMenu("shop")}}>Shop</li>
+        <li onClick={()=>{setMenu("ladies")}}>Ladies</li>
+        <li onClick={()=>{setMenu("gentlemen")}}>Gentlemen</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids</li>
     </ul>
+    <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="cart-icon"/>
+        <div className='nav-cart-count'>0</div>
+    </div>
     </div>
   )
 }
